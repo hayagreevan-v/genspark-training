d53c70ff23c23527baaed4f59271713fc8d9aff6
hayagreevan-v <hayagree@gmail.com>
2025-07-29 14:46:12 +0530

    day-60

---
+++ 2025-07-25/ClientVHShop/Program.cs
@@ -0,0 +1,58 @@
+using ChienVHShopOnline.Contexts;
+using ChienVHShopOnline.Models;
+using ChienVHShopOnline.Repositories;
+using ChienVHShopOnline.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddJsonOptions(
+    options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    }
+);
+
+builder.Services.AddDbContext<ChienVHShopDBEntities>(
+    options =>
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
+    }
+);
+
+builder.Services.AddTransient<CategoryRepo>();
+builder.Services.AddTransient<ColorRepo>();
+builder.Services.AddTransient<ContactURepo>();
+builder.Services.AddTransient<ModelRepo>();
+builder.Services.AddTransient<NewsRepo>();
+builder.Services.AddTransient<OrderDetailRepo>();
+builder.Services.AddTransient<OrderRepo>();
+builder.Services.AddTransient<ProductRepo>();
+builder.Services.AddTransient<UserRepo>();
+
+builder.Services.AddTransient<CategoryService>();
+builder.Services.AddTransient<ColorService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+
+app.Run();
+
