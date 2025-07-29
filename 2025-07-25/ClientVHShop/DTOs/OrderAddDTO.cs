490091f4522a95dfb9d27c7584af3a7085fa6612
hayagreevan-v <hayagree@gmail.com>
2025-07-29 15:08:36 +0530

    Migration Project

---
+++ 2025-07-25/ClientVHShop/DTOs/OrderAddDTO.cs
@@ -0,0 +1,13 @@
+namespace ChienVHShopOnline.DTOs;
+
+public class OrderAddDTO
+{
+    public string OrderName { get; set; } = string.Empty;
+    public System.DateTime? OrderDate { get; set; } = DateTime.UtcNow;
+    public string PaymentType { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public string CustomerName { get; set; } = string.Empty;
+    public string CustomerPhone { get; set; } = string.Empty;
+    public string CustomerEmail { get; set; } = string.Empty;
+    public string CustomerAddress { get; set; } = string.Empty;
+}
